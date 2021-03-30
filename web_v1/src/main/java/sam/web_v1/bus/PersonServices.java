a4223c61b34f75903686f16023b717729a41b0ee
Sam2303 <sjp2000@live.com>
2021-03-30 19:12:07 +0100

    first upload

---
+++ web_v1/src/main/java/sam/web_v1/bus/PersonServices.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sam.web_v1.bus;
+
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import sam.web_v1.ent.Person;
+import sam.web_v1.pers.PersonFacade;
+
+/**
+ *
+ * @author sjp20
+ */
+@Stateless
+public class PersonServices {
+    
+    @EJB
+    private PersonFacade pf;
+    
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+    
+    public Person createNewPerson(Person p){
+        pf.create(p);
+        return p;
+    }
+}
---
+++ web_v1/src/main/java/sam/web_v1/ctrl/PersonCtrl.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sam.web_v1.ctrl;
+
+import javax.ejb.EJB;
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+import sam.web_v1.bus.PersonServices;
+import sam.web_v1.ent.Person;
+
+/**
+ *
+ * @author sjp20
+ */
+@Named(value = "personCtrl")
+@RequestScoped
+public class PersonCtrl {
+    
+    private Person p = new Person();
+
+    public Person getP() {
+        return p;
+    }
+
+    public void setP(Person p) {
+        this.p = p;
+    }
+    
+    
+    public PersonCtrl() {
+    }
+    
+    @EJB
+    private PersonServices ps;
+    public String doCreatePerson() {
+        ps.createNewPerson(p);
+        return "";
+    }
+    
+}
