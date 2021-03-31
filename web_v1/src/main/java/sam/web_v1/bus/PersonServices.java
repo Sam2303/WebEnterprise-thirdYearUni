07cd73ee929fe000242b38aab025a38e8e45ed64
Sam2303 <sjp2000@live.com>
2021-03-31 12:18:15 +0100

    satrted register

--- web_v1/src/main/java/sam/web_v1/bus/PersonServices.java
+++ web_v1/src/main/java/sam/web_v1/bus/PersonServices.java
@@ -27,4 +27,5 @@ public class PersonServices {
         pf.create(p);
         return p;
     }
+   
 }
--- web_v1/src/main/java/sam/web_v1/ent/Person.java
+++ web_v1/src/main/java/sam/web_v1/ent/Person.java
@@ -21,11 +21,57 @@ public class Person implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    
     private Long id;
+    private String userName;
+    private String password;    
+    private String firstName;
     private String lastName;
+    private String phoneNumber;
+    private String emailAddress;
 
     public String getLastName() {
         return lastName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
     }
 
     public void setLastName(String lastName) {
